03e962f0f3cc2c5ebcf162d41a6dfbd097d243ca
Julio <juliocesar13b@gmail.com>
2025-08-24 13:16:22 -0300

    finalizando entidades

--- src/main/java/com/microservico/vendas/Entities/ItemPedido.java
+++ src/main/java/com/microservico/vendas/Entities/ItemPedido.java
@@ -2,6 +2,8 @@ package com.microservico.vendas.Entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_item_pedido")
 public class ItemPedido {
@@ -17,4 +19,67 @@ public class ItemPedido {
     @JoinColumn(name = "pedido_id")
     private Pedido pedido;
 
+
+    public ItemPedido() {
+    }
+
+    public ItemPedido(Long id, Long produtoId, int quantidade, int subtotal, Pedido pedido) {
+        this.id = id;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.subtotal = subtotal;
+        this.pedido = pedido;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Long produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(int subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemPedido that = (ItemPedido) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/com/microservico/vendas/Entities/Pedido.java
+++ src/main/java/com/microservico/vendas/Entities/Pedido.java
@@ -3,6 +3,8 @@ package com.microservico.vendas.Entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -16,5 +18,79 @@ public class Pedido {
     private LocalDateTime dataPedido;
     private Status status;
     private Double valorTotal;
-    private List<Itens> itensList;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itensList;
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, UUID clientId, LocalDateTime dataPedido, Status status, Double valorTotal, List<ItemPedido> itensList) {
+        this.id = id;
+        this.clientId = clientId;
+        this.dataPedido = dataPedido;
+        this.status = status;
+        this.valorTotal = valorTotal;
+        this.itensList = itensList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
+    }
+
+    public LocalDateTime getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(LocalDateTime dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public List<ItemPedido> getItensList() {
+        return itensList;
+    }
+
+    public void setItensList(List<ItemPedido> itensList) {
+        this.itensList = itensList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pedido pedido = (Pedido) o;
+        return Objects.equals(id, pedido.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
