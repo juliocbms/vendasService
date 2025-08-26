841cbcaae0d65b528e6ffa1a1dd9ea2593710f94
Julio <juliocesar13b@gmail.com>
2025-08-25 22:10:06 -0300

    ajustando servico de salvar pedido

--- src/main/java/com/microservico/vendas/Services/PedidoService.java
+++ src/main/java/com/microservico/vendas/Services/PedidoService.java
@@ -41,41 +41,34 @@ public class PedidoService {
         BigDecimal valorTotalDoPedido = BigDecimal.ZERO;
         for (ItemPedido item : pedido.getItensList()) {
             try {
-                System.out.println("Buscando preço para o produto ID: " + item.getProdutoId());
-                ResponseEntity<BigDecimal> responsePreco = estoqueCliente.getPriceById(item.getProdutoId());
-                BigDecimal precoUnitario = responsePreco.getBody();
-                BigDecimal subtotalItem = precoUnitario.multiply(BigDecimal.valueOf(item.getQuantidade()));
-                item.setSubtotal(subtotalItem);
-                valorTotalDoPedido = valorTotalDoPedido.add(subtotalItem);
-
-            } catch (FeignException e) {
-                if (e.status() == 404) {
-                    throw new ResourceNotFoundException("O produto com ID " + item.getProdutoId() + " não foi encontrado para cálculo de preço.");
-                } else {
-                    throw new DatabaseException("Erro ao buscar preço para o produto ID " + item.getProdutoId());
-                }
-            }
-        }
-        pedido.setValorTotal(valorTotalDoPedido);
-        for (ItemPedido item : pedido.getItensList()) {
-            try {
                 System.out.println("Validando estoque para o produto ID: " + item.getProdutoId() + ", Quantidade: " + item.getQuantidade());
                 estoqueCliente.validarDisponibilidade(
                         item.getProdutoId(),
                         item.getQuantidade()
                 );
+
+                System.out.println("Buscando preço para o produto ID: " + item.getProdutoId());
+                ResponseEntity<BigDecimal> responsePreco = estoqueCliente.getPriceById(item.getProdutoId());
+                BigDecimal precoUnitario = responsePreco.getBody();
+
+                BigDecimal subtotalItem = precoUnitario.multiply(BigDecimal.valueOf(item.getQuantidade()));
+                item.setSubtotal(subtotalItem);
+                valorTotalDoPedido = valorTotalDoPedido.add(subtotalItem);
+                item.setPedido(pedido);
+
             } catch (FeignException e) {
                 if (e.status() == 404) {
+                    throw new ResourceNotFoundException("O produto com ID " + item.getProdutoId() + " não foi encontrado.");
+                } else if (e.status() == 400) {
                     throw new EstoqueInsuficienteException("Estoque insuficiente para o produto com ID " + item.getProdutoId());
                 } else {
-                    throw new DatabaseException("Erro ao validar estoque para o produto ID " + item.getProdutoId());
+                    throw new DatabaseException("Ocorreu um erro na comunicação com o serviço de estoque para o produto ID " + item.getProdutoId());
                 }
             }
         }
-
-        for (ItemPedido item : pedido.getItensList()) {
-            item.setPedido(pedido);
-        }
+        pedido.setValorTotal(valorTotalDoPedido);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setStatus(Status.PENDENTE);
 
         Pedido pedidoSalvo = pedidoRepository.save(pedido);
         rabbitTemplate.convertAndSend(exchangeVendas, "pedido.criado", pedidoSalvo);
